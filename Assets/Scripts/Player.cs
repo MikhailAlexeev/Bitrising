58e84310c6970b7d6ada7d08b4536de6211d7916
Mikhail Alexeev <cuvemots@gmail.com>
2020-03-13 00:02:57 +0300

    Proto movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public int moveForce;
+    public int jumpForce;
+    private Rigidbody2D _rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerControl();
+    }
+
+    public void MoveLeft()
+    {
+        _rb.AddForce(Vector2.left * (moveForce * Time.deltaTime), ForceMode2D.Force);
+    }
+    
+    public void MoveRight()
+    {
+        _rb.AddForce(Vector2.right * (moveForce * Time.deltaTime), ForceMode2D.Force);
+    }
+
+    public void Jump()
+    {
+        _rb.AddForce(Vector2.up * (jumpForce * Time.deltaTime), ForceMode2D.Force);
+    }
+
+    public void PlayerControl()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+        
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+    
+}
