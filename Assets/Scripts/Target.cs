5f50e8b1ee6ef1734e8939c035a2b21642e4f58b
Mikhail Alexeev <cuvemots@gmail.com>
2020-03-17 01:14:50 +0300

    Target

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Target : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.GetComponentInParent<Player>())
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
