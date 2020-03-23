d75484b395949748f196a0371bef041a5f826876
Mikhail Alexeev <cuvemots@gmail.com>
2020-03-23 23:05:27 +0300

    Button logic

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,107 +1,111 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using DG.Tweening;
+﻿using DG.Tweening;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class Player : MonoBehaviour
-{
-    public int moveForce;
-    public Joystick joystick;
-    [Header("Jump controls")] public float jumpForce;
-    public float fallMultiplier;
-    public float lowJumpMultiplier;
-    [Header("Jump Punch Effect")] public Vector2 scale;
-    public float duration;
-    public float elasticity;
-
-    private TriggerDetector triggerDetector;
-
-
-    private Rigidbody2D rb;
-    private float horizontalInput;
-    private bool jumpInput;
-
-    void Start()
+    public class Player : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody2D>();
-        triggerDetector = GetComponentInChildren<TriggerDetector>();
-    }
+        public int moveForce;
+        public Joystick joystick;
+        public Button joystickButton;
+        [Header("Jump controls")] public float jumpForce;
+        public float fallMultiplier;
+        public float lowJumpMultiplier;
+        [Header("Jump Punch Effect")] public Vector2 scale;
+        public float duration;
+        public float elasticity;
 
-    void Update()
-    {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        if (Input.GetKeyDown(KeyCode.Space)) jumpInput = true;
-        else jumpInput = false;
+        private TriggerDetector triggerDetector;
 
-        JoystickMove();
-    }
 
-    private void FixedUpdate()
-    {
-        MoveHorizontal();
-        Jump();
-        JumpAmplifier();
-    }
+        private Rigidbody2D rb;
+        private float horizontalInput;
+        private bool jumpInput;
 
-    void MoveHorizontal()
-    {
-        rb.velocity += new Vector2(horizontalInput, 0) * (moveForce * Time.deltaTime);
-    }
-
-    void JoystickMove()
-    {
-        rb.AddForce(new Vector2(joystick.Horizontal * moveForce, 0.0f));
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.transform.GetComponentInParent<Platform>() != null)
+        void Start()
         {
-            transform.SetParent(other.transform);
+            rb = GetComponent<Rigidbody2D>();
+            triggerDetector = GetComponentInChildren<TriggerDetector>();
         }
-    }
 
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.transform.GetComponentInParent<Platform>() != null)
+        void Update()
         {
-            transform.SetParent(null);
+            horizontalInput = Input.GetAxisRaw("Horizontal");
+            jumpInput = Input.GetKeyDown(KeyCode.Space);
+
+            JoystickMove();
         }
-    }
 
-
-    void Jump()
-    {
-        if (jumpInput)
+        private void FixedUpdate()
         {
-            if (triggerDetector.InTrigger)
+            MoveHorizontal();
+            Jump();
+            JumpAmplifier();
+        }
+
+        void MoveHorizontal()
+        {
+            rb.velocity += new Vector2(horizontalInput, 0) * (moveForce * Time.deltaTime);
+        }
+
+        void JoystickMove()
+        {
+            rb.AddForce(new Vector2(joystick.Horizontal * moveForce, 0.0f));
+        }
+
+        public void JoystickPressed()
+        {
+            jumpInput = true;
+            Jump();
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.transform.GetComponentInParent<Platform>() != null)
             {
-                JumpPunch();
-                rb.AddForce(Vector2.up * jumpForce);
+                transform.SetParent(other.transform);
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if (other.transform.GetComponentInParent<Platform>() != null)
+            {
                 transform.SetParent(null);
             }
         }
-    }
 
-    void JumpPunch()
-    {
-        transform.DOPunchScale(scale, duration, elasticity: elasticity);
-    }
 
-    void JumpAmplifier()
-    {
-        if (rb.velocity.y < 0)
+        void Jump()
         {
-            rb.velocity += Vector2.up * (Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime);
+            if (jumpInput)
+            {
+                if (triggerDetector.InTrigger)
+                {
+                    JumpPunch();
+                    rb.AddForce(Vector2.up * jumpForce);
+                    transform.SetParent(null);
+                }
+            }
+        }
 
-            // Get this to work with joystick button
-            // else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
-            // {
-            //     rb.velocity += Vector2.up * (Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime);
-            // }
+        void JumpPunch()
+        {
+            transform.DOPunchScale(scale, duration, elasticity: elasticity);
+        }
+
+        void JumpAmplifier()
+        {
+            if (rb.velocity.y < 0)
+            {
+                rb.velocity += Vector2.up * (Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime);
+
+                // Get this to work with joystick button
+                // else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+                // {
+                //     rb.velocity += Vector2.up * (Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime);
+                // }
+            }
         }
     }
-}
