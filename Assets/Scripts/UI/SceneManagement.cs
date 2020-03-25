ecb4a6580dbfd3d1be22b078bbf387c8d376938a
Mikhail Alexeev <cuvemots@gmail.com>
2020-03-26 02:09:52 +0300

    Scene change logic

---
+++ Assets/Scripts/UI/SceneManagement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneManagement : MonoBehaviour
+{
+    public enum OnRestartAction
+    {
+        LoadThisScene,
+        LoadNextScene
+    }
+
+    public OnRestartAction onRestartAction;
+
+    private Scene currentScene;
+
+    private void Awake()
+    {
+        currentScene = SceneManager.GetActiveScene();
+    }
+
+    public void Restart()
+    {
+        switch (onRestartAction)
+        {
+            case OnRestartAction.LoadThisScene:
+                SceneManager.LoadScene(currentScene.buildIndex);
+                break;
+            case OnRestartAction.LoadNextScene:
+                if (currentScene.buildIndex < SceneManager.sceneCountInBuildSettings-1)
+                {
+                    SceneManager.LoadScene(currentScene.buildIndex + 1);
+                }
+                else
+                {
+                    SceneManager.LoadScene(0);
+                }
+                break;
+        }
+    }
+}
