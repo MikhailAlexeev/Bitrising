68dfe7a81b41d10a82ca568396aa59a99cf1ffae
Mikhail Alexeev <cuvemots@gmail.com>
2020-03-26 01:28:02 +0300

    Changed offscreen death logic to triggers

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,111 +1,122 @@
-﻿using DG.Tweening;
+﻿using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 
 [RequireComponent(typeof(Rigidbody2D))]
-    public class Player : MonoBehaviour
+public class Player : MonoBehaviour
+{
+    public int moveForce;
+    public Joystick joystick;
+    public Button joystickButton;
+    [Header("Jump controls")] public float jumpForce;
+    public float fallMultiplier;
+    public float lowJumpMultiplier;
+    [Header("Jump Punch Effect")] public Vector2 scale;
+    public float duration;
+    public float elasticity;
+    [Header("UI")] public Button restartButton;
+
+    private TriggerDetector triggerDetector;
+
+
+    private Rigidbody2D rb;
+    private float horizontalInput;
+    private bool jumpInput;
+
+    void Start()
     {
-        public int moveForce;
-        public Joystick joystick;
-        public Button joystickButton;
-        [Header("Jump controls")] public float jumpForce;
-        public float fallMultiplier;
-        public float lowJumpMultiplier;
-        [Header("Jump Punch Effect")] public Vector2 scale;
-        public float duration;
-        public float elasticity;
+        rb = GetComponent<Rigidbody2D>();
+        triggerDetector = GetComponentInChildren<TriggerDetector>();
+    }
 
-        private TriggerDetector triggerDetector;
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        jumpInput = Input.GetKeyDown(KeyCode.Space);
 
+        JoystickMove();
+    }
 
-        private Rigidbody2D rb;
-        private float horizontalInput;
-        private bool jumpInput;
+    private void FixedUpdate()
+    {
+        MoveHorizontal();
+        Jump();
+        JumpAmplifier();
+    }
 
-        void Start()
+    void MoveHorizontal()
+    {
+        rb.velocity += new Vector2(horizontalInput, 0) * (moveForce * Time.deltaTime);
+    }
+
+    void JoystickMove()
+    {
+        rb.AddForce(new Vector2(joystick.Horizontal * moveForce, 0.0f));
+    }
+
+    public void JoystickPressed()
+    {
+        jumpInput = true;
+        Jump();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.GetComponentInParent<Platform>() != null)
         {
-            rb = GetComponent<Rigidbody2D>();
-            triggerDetector = GetComponentInChildren<TriggerDetector>();
+            transform.SetParent(other.transform);
         }
+    }
 
-        void Update()
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("DeathZone"))
         {
-            horizontalInput = Input.GetAxisRaw("Horizontal");
-            jumpInput = Input.GetKeyDown(KeyCode.Space);
-
-            JoystickMove();
+            restartButton.gameObject.SetActive(true);
+            gameObject.SetActive(false);
         }
+    }
 
-        private void FixedUpdate()
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.transform.GetComponentInParent<Platform>() != null)
         {
-            MoveHorizontal();
-            Jump();
-            JumpAmplifier();
+            transform.SetParent(null);
         }
+    }
 
-        void MoveHorizontal()
-        {
-            rb.velocity += new Vector2(horizontalInput, 0) * (moveForce * Time.deltaTime);
-        }
 
-        void JoystickMove()
+    void Jump()
+    {
+        if (jumpInput)
         {
-            rb.AddForce(new Vector2(joystick.Horizontal * moveForce, 0.0f));
-        }
-
-        public void JoystickPressed()
-        {
-            jumpInput = true;
-            Jump();
-        }
-
-        private void OnCollisionEnter2D(Collision2D other)
-        {
-            if (other.transform.GetComponentInParent<Platform>() != null)
+            if (triggerDetector.InTrigger)
             {
-                transform.SetParent(other.transform);
-            }
-        }
-
-        private void OnCollisionExit2D(Collision2D other)
-        {
-            if (other.transform.GetComponentInParent<Platform>() != null)
-            {
+                JumpPunch();
+                rb.AddForce(Vector2.up * jumpForce);
                 transform.SetParent(null);
             }
         }
+    }
 
+    void JumpPunch()
+    {
+        transform.DOPunchScale(scale, duration, elasticity: elasticity);
+    }
 
-        void Jump()
+    void JumpAmplifier()
+    {
+        if (rb.velocity.y < 0)
         {
-            if (jumpInput)
-            {
-                if (triggerDetector.InTrigger)
-                {
-                    JumpPunch();
-                    rb.AddForce(Vector2.up * jumpForce);
-                    transform.SetParent(null);
-                }
-            }
-        }
+            rb.velocity += Vector2.up * (Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime);
 
-        void JumpPunch()
-        {
-            transform.DOPunchScale(scale, duration, elasticity: elasticity);
-        }
-
-        void JumpAmplifier()
-        {
-            if (rb.velocity.y < 0)
-            {
-                rb.velocity += Vector2.up * (Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime);
-
-                // Get this to work with joystick button
-                // else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
-                // {
-                //     rb.velocity += Vector2.up * (Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime);
-                // }
-            }
+            // Get this to work with joystick button
+            // else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+            // {
+            //     rb.velocity += Vector2.up * (Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime);
+            // }
         }
     }
+}
--- Assets/Scripts/UI/UILogic.cs
+++ Assets/Scripts/UI/UILogic.cs
@@ -6,24 +6,6 @@ using UnityEngine.UI;
 
 public class UILogic : MonoBehaviour
 {
-    public Player player;
-    public Button restartButton;
-    
-    void Update()
-    {
-        PlayerOffScreen();
-    }
-
-    public void PlayerOffScreen()
-    {
-        if (player.transform.position.x < -10.3f || 10.3f < player.transform.position.x)
-        {
-            player.gameObject.SetActive(false);
-            restartButton.gameObject.SetActive(true);
-        }
-     
-    }
-    
     public void Restart()
     {
         SceneManager.LoadScene(0);
