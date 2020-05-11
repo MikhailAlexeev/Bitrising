c8fcde4a063e29dc4a9a18b03c5f433f62fce0b4
Mikhail Alexeev <cuvemots@gmail.com>
2020-05-11 15:06:23 +0300

    Modified DeathZone behaviour

---
+++ Assets/Scripts/Behaviours/DeathZoneBehaviour.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class DeathZoneBehaviour : MonoBehaviour
+{
+public Transform player;
+private bool _isplayerNotNull;
+
+
+// Update is called once per frame
+    private void Start()
+    {
+        _isplayerNotNull = player!=null;
+    }
+
+    void Update()
+    {
+        if (_isplayerNotNull)
+        {
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, 0.0f);
+        }
+    }
+}
